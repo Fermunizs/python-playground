d5725257615d110f5a995bcce2ae15971659a3b9
Fermunizs <munizsilvestrefernanda@gmail.com>
2026-08-03 22:47:44 -0300

    Está sendo configurada a solução utilizando o dict

--- challenges/two-sum/dict.py
+++ challenges/two-sum/dict.py
@@ -1,4 +1,8 @@
 values = [1, 14, 9, 7, 2, 11, 0]
-memoria = {}
 target = int(input("Digite o valor do orçamento: "))
 
+def two_values(values, target):
+    for i in range(len(values)):
+        memoria = {}
+        resto = target - values[i]
+        memoria[values[i]] = i
