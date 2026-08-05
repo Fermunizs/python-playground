e3f9097060898854afe45ee89d69a5964e81db7b
Fermunizs <munizsilvestrefernanda@gmail.com>
2026-08-04 21:03:19 -0300

    Foi realizada a otimização do código adicionando a variável complemento.

--- challenges/two-sum/dict.py
+++ challenges/two-sum/dict.py
@@ -5,8 +5,9 @@ def two_values(values, target):
     memoria = {}
     for i in range(len(values)):
         if target - values[i] in memoria:
-            print(f"Valores encontrados! {values[i]} + {target - values[i]} = {target}, os index deles são respectivamente {i} e {memoria[target - values[i]]}")
-            return (values[i], target - values[i])
+            complemento = target - values[i]
+            print(f"Valores encontrados! {values[i]} + {complemento} = {target}, os index deles são respectivamente {i} e {complemento}.")
+            return (i , complemento) 
         else:
             memoria[values[i]] = i
             print(f"{values[i]} foi adicionado à memória.")
